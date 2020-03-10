76f8d6577190f2aa6f7eda5cfb8dfd296bbe7f32
Henry Lee <hlee2052@gmail.com>
2020-03-09 22:19:38 -0700

    search by query object done

--- index.js
+++ index.js
@@ -152,31 +152,29 @@ let newQuery = {
      */
     "AND": [
         {
-            name: "Luke",
-            name_exact: true
+            name: "Sky",
+            name_exact: false
         },
         {
-            height_lo: 100,
-            height_hi: 140
+            height_lo: 170,
+            height_hi: 200
         },
         {
-            hair_color: 'blue'
+            mass_lo: 77,
+            mass_hi: 79
         },
         {
-            mass_lo: 10,
-            mass_hi: 10
+            gender: 'male'
         }
+        /*
+            {
+                hair_color: 'blue'
+            },
+           */
 
     ],
     "OR": [
-        {
-            skin_color: "pale"
-        },
-        {
-            eye_color: "blue"
-        },
-        {gender: 'male'}
-
+        {gender: 'n/a'}
     ]
 }
 
@@ -204,29 +202,140 @@ const queryList = [
 //eg: name contains "Skywalker'  or height(between 120-190) or mass =77 and hair color_blond and eye_color blue
 //  and any gender
 
-function searchByParam(query) {
+
+function searchCharacter(query) {
     return new Promise((resolve, reject) => {
         let result = []
+        let set = new Set();
+        if (query === undefined) reject(result)
 
         let andList = query['AND']
         let orList = query['OR']
         if ((andList === undefined || !Array.isArray(andList)) || (orList === undefined) || !Array.isArray(orList)) {
             //  At least one AND or OR clause must exist!
+            reject(result)
         }
-
         /*
         1. create OR query
         2. run the query
          */
+        // loop through characters
+        for (let i = 0; i < characterArray.length; i++) {
+            let currentChar = characterArray[i]
+            // all list must be true;
+            if (andList.length === 0) {
+                // this object should not be added;
+                continue;
+            }
 
+            let andClause = true;
+            for (let curr of andList) {
+                andClause = checkEachClause(curr, currentChar)
+                if (!andClause) {
+                    // this object does not satisify and clause
+                    break;
+                }
+            }
 
+            let orClause = false;
+            for (let curr of orList) {
+                orClause |= checkEachClause(curr, currentChar)
+                if (orClause) break;
+            }
+
+            if ((andClause || orClause) && !set.has(currentChar["name"])) {
+                result.push(currentChar);
+                set.add(currentChar["name"])
+            }
+        }
+        // console.log(result)
+        resolve(result)
     })
 
 }
+
+function checkEachClause(currAnd, currentChar) {
+    // each AND object must only contain EITHER one or two, thus check length...
+    // eg) invalid: {name:"Luke", height_lo: "..."} --> name and height cannot be together
+    if ("name" in currAnd) {
+        return checkName(currAnd, currentChar);
+    } else if ("height_lo" in currAnd) {
+        return checkHeight(currAnd, currentChar)
+    } else if ("mass_lo" in currAnd) {
+        return checkMass(currAnd, currentChar)
+    } else if ("hair_color" in currAnd) {
+        return checkHairColor(currAnd, currentChar)
+    } else if ("skin_color" in currAnd) {
+        return checkSkinColor(currAnd, currentChar)
+    } else if ("eye_color" in currAnd) {
+        return checkEyeColor(currAnd, currentChar)
+    } else if ("gender" in currAnd) {
+        return checkGender(currAnd, currentChar)
+    }
+    return false;
+}
+
+
+function checkName(clause, currentChar) {
+    // only two clauses name, name_exact
+    if (Object.keys(clause).length !== 2 || !"name_exact" in clause) return false;
+    if (clause["name_exact"]) {
+        return (currentChar["name"] === clause["name"])
+    } else {
+        return currentChar["name"].includes(clause["name"])
+    }
+}
+
+function checkHeight(clause, currentChar) {
+    if (Object.keys(clause).length !== 2 || !"height_hi" in clause) return false;
+
+    let currentCharHeight = currentChar['height'];
+    let loLimit = clause['height_lo']
+    let hiLimit = clause['height_hi']
+
+    return currentCharHeight >= loLimit && currentCharHeight <= hiLimit;
+}
+
+function checkMass(clause, currentChar) {
+    if (Object.keys(clause).length !== 2 || !"mass_hi" in clause) return false;
+
+    let currentCharHeight = currentChar['mass'];
+    let loLimit = clause['mass_lo']
+    let hiLimit = clause['mass_hi']
+
+    return currentCharHeight >= loLimit && currentCharHeight <= hiLimit;
+}
+
+function checkHairColor(clause, currentChar) {
+    if (Object.keys(clause).length !== 1) return false;
+    return currentChar['hair_color'] === clause['hair_color']
+}
+
+function checkSkinColor(clause, currentChar) {
+    if (Object.keys(clause).length !== 1) return false;
+    return currentChar['skin_color'] === clause['skin_color']
+}
+
+function checkEyeColor(clause, currentChar) {
+    if (Object.keys(clause).length !== 1) return false;
+    return currentChar['eye_color'] === clause['eye_color']
+}
+
+function checkGender(clause, currentChar) {
+    if (Object.keys(clause).length !== 1) return false;
+    return currentChar['gender'] === clause['gender']
+}
+
+searchCharacter(newQuery).then(result => {
+    console.log(result)
+}).catch(err => {
+    console.log(err)
+})
 
 
 module.exports = {
     searchByHeight,
     searchByMass,
-    searchByGender
+    searchByGender,
+    searchCharacter
 }
